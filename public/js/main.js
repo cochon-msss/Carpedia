9c1a6b5ff54be2e3a972edcd794ec980e0ee7bb9
손민성 <son344910@naver.com>
2025-03-19 22:06:37 +0900

    첫 화면 디자인 수정

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,37 @@ $(() => {
   $(".car-search").on("click", function () {
     window.location.href = `/carInfo`;
   });
+  $(document).ready(function () {
+    $(".custom-dropdown").click(function (event) {
+      event.stopPropagation();
+      $(".custom-dropdown").not(this).removeClass("open");
+      $(this).toggleClass("open");
+
+      // search-form의 가로 크기를 가져와서 dropdown-list에 적용
+      let searchFormWidth = $(".search-form").outerWidth();
+      let dropdownList = $(this).find(".dropdown-list");
+
+      dropdownList.css("width", searchFormWidth + "px");
+
+      // 드롭다운 보이게 하기
+      if ($(this).hasClass("open")) {
+        dropdownList.addClass("open");
+      } else {
+        dropdownList.removeClass("open");
+      }
+    });
+
+    $(".dropdown-list li").click(function (event) {
+      event.stopPropagation();
+      let selectedText = $(this).text();
+      $(this).closest(".custom-dropdown").find(".selected").text(selectedText);
+      $(this).closest(".custom-dropdown").removeClass("open");
+      $(this).closest(".dropdown-list").removeClass("open");
+    });
+
+    $(document).click(function () {
+      $(".custom-dropdown").removeClass("open");
+      $(".dropdown-list").removeClass("open");
+    });
+  });
 });
