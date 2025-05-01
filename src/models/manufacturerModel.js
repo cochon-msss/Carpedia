0ead1b1fcc7eb2b1012fdb6c31bab9d3bc6a58ce
손민성 <son344910@naver.com>
2025-05-01 15:27:10 +0900

    add background images

--- src/models/manufacturerModel.js
+++ src/models/manufacturerModel.js
@@ -1,11 +1,18 @@
 const dbHelper = require("../utils/dbHelper");
 
 const getManufacturerList = async () => {
-  return (
-    await dbHelper.query(
-      "SELECT id, name, country_code AS countryCode, founded_year AS foundedYear, logo_url AS logoUrl, create_at AS createAt FROM manufacturer"
-    )
-  ).rows;
+  return await dbHelper.query(
+    `SELECT manufacturer_seq AS manufacturerSeq,
+              manufacturer_name AS manufacturerName,
+              country_code AS countryCode,
+              founded_year AS foundedYear,
+              logo_url AS logoUrl,
+              create_at AS createAt
+        FROM manufacturer
+        ORDER BY 
+          CASE countryCode WHEN 'KR' THEN 0
+          END DESC, manufacturerName`
+  );
 };
 const getModelList = async (manufacturerId) => {
   return (
--- src/utils/dbHelper.js
+++ src/utils/dbHelper.js
@@ -2,7 +2,8 @@ const db = require("../config/db");
 
 const query = async (sql, params = []) => {
   try {
-    return await db.query(sql, params);
+    const [rows] = await db.query(sql, params);
+    return rows; // 여기서 rows만 반환
   } catch (error) {
     throw new Error("Database query failed : " + error.message); // 에러를 던짐
   }
